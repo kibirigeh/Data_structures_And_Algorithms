ac000b8ee91eb26df6abc9f3d43af0b24a66d99d
Haruna Kibirige <HarunaKibirige@fico.com>
2015-03-20 14:59:38 -0500

    Created subset.java which is atest class of using a randomized queue to solve a problem

---
+++ Data Structures and Algorithms/src/algorithms_Course_1/Subset.java
@@ -0,0 +1,38 @@
+package algorithms_Course_1;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+/*
+ * 
+ * Write a client program Subset.java that takes a command-line integer k; reads in a sequence of N strings from standard input using StdIn.readString();
+ * and prints out exactly k of them, uniformly at random.
+ * Each item from the sequence can be printed out at most once. You may assume that 0 <= k <= N, where N is the number of string on standard input.
+ */
+
+public class Subset {
+
+	static int k;
+	static RandomizedQueue<String> randQ;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length>=1)
+		{
+			k=Integer.parseInt(args[0]);
+			randQ=new RandomizedQueue<String>();
+			
+			// read strings from std input:
+			String [] s = StdIn.readLine().split(" ");
+	        for(String i: s)
+	        {
+	             randQ.enqueue(i);
+	        }
+		
+			for(int i=0;i<k;i++){
+				StdOut.println(randQ.dequeue());
+			}
+		}
+	}
+
+}
