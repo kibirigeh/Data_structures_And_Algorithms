2be784ab2c6b620060c8a9453892e23462dc8d9a
kibirigeh <kibirige0414@yahoo.com>
2015-05-06 23:13:26 -0500

    Added the book solution to unique string validation

--- Data Structures and Algorithms/src/cracking_Coding_Interviews/UniqueString.java
+++ Data Structures and Algorithms/src/cracking_Coding_Interviews/UniqueString.java
@@ -4,11 +4,13 @@ public class UniqueString {
 
 	/*
 	 *algorithm to test if all characters in string are unique 
+	 *stringUnique1 uses extra datastructure
+	 *stringUnique2 does not
 	 */
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean test = stringUnique("aa");
+		boolean test = stringUnique2("tes");
 		if(test)
 		{
 			System.out.println("Unique is true");
@@ -28,6 +30,19 @@ public class UniqueString {
 			int freq = (!contains)?(1):(freqMap.get(""+word.charAt(i))+1);
 			if(freq>1)return false;
 			freqMap.put(""+word.charAt(i),freq);
+		}
+		return true;
+	}
+	
+	public static boolean stringUnique2(String word)//method that uses a boolean array of the 256 (possible unique asci characters) to verify uniqueness	
+	{
+		if(word.length()>256)return false;
+		boolean [] char_set = new boolean[256];
+		for(int i=0;i<word.length();i++)
+		{
+			int val = word.charAt(i);
+			if(char_set[val])return false;
+			char_set[val]=true;
 		}
 		return true;
 	}
