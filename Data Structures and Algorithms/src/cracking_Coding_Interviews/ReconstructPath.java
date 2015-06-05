cfd73ec34dd7e4db75e3e5ec0e1f682e9a785e82
kibirigeh <kibirige0414@yahoo.com>
2015-06-05 00:08:28 -0500

    Added Career Cup question for re-constructing an itinerary

---
+++ Data Structures and Algorithms/src/cracking_Coding_Interviews/ReconstructPath.java
@@ -0,0 +1,41 @@
+package cracking_Coding_Interviews;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class ReconstructPath {
+	/*
+	 * 
+	 * Find the start point in a map of Trips 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String,String>trips = new HashMap<String,String>();
+		trips.put("MUC","LHR");
+		trips.put("CDG","MUC");
+		trips.put("SFO","SJC");
+		trips.put("LHR","SFO");
+		for(String point: reconstruct(trips)){
+			System.out.println(point);
+		}
+	}
+	/*
+	 * This finds which origination is not a destination in the map
+	 */
+	public static String[] reconstruct(HashMap<String,String>trips){
+		String [] paths = new String[trips.size()+1];
+		String startK;
+		for(Entry<String,String> trip : trips.entrySet() ){
+			if(trips.containsValue(trip.getKey()))continue;
+			paths[0]=trip.getKey();
+			break;
+		}
+		
+		for(int i=0;i<paths.length-1;i++){
+			paths[i+1]=trips.get(paths[i]);
+		}
+		
+		return paths;
+	}
+
+}
