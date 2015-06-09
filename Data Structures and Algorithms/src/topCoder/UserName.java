8433ae22bf42401ff20e756412a81f2fae25d7a4
kibirigeh <kibirige0414@yahoo.com>
2015-06-08 22:13:04 -0500

    added username from topcoder

--- Data Structures and Algorithms/src/topCoder/UserName.java
+++ Data Structures and Algorithms/src/topCoder/UserName.java
@@ -5,18 +5,24 @@ import java.util.*;
 public class UserName{
 	public static String newMember(String[] names,String pref){
 		int x=1;
+		String result=pref;
 		Arrays.sort(names);
+		boolean found = false;
 		for(int i=0;i<names.length;i++){
-			if(names[i]==pref){
-				pref+=(x++);
+			if(names[i].equalsIgnoreCase(result)){
+				result=pref+(x++);
+				found=true;
+			}
+			else if(found){
+				break;
 			}
 		}
-		return pref;
+		return result;
 	}
 	
-	static void main(String[] args){
+	public static void main(String[] args){
 		String[] names ={"MasterOfDisaster", "TygerTyger1", "DingBat", "Orpheus", 
-				 "TygerTyger", "WolfMan", "MrKnowItAll"};
+				 "TygerTyger", "WolfMan","WolfMan2", "MrKnowItAll"};
 		String pref ="TygerTyger";
 		System.out.println(newMember(names,pref));
 	}
