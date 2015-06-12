24d3706fcb5c804de7023e4ea4f17381ceffc4a6
kibirigeh <kibirige0414@yahoo.com>
2015-06-11 23:23:42 -0500

    added fixes to solme top coder problems

--- Data Structures and Algorithms/src/topCoder/FoxAndMp3Easy.java
+++ Data Structures and Algorithms/src/topCoder/FoxAndMp3Easy.java
@@ -1,34 +1,26 @@
 package topCoder;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FoxAndMp3Easy {
 
 	public String[] playList(int n){
-		String [] results = new String[((n>50)?(50):(n))];
-		int i=0,j=1,k=1;
-		Matcher m ;
-		String ext=".mp3";
-		StringBuilder sb ;
+		String [] nums = new String[n];
 		
-		while(i<n && i<50){
-			if(k<=n){
-				System.out.println("J: "+j+" K: "+k);
-				m = Pattern.compile("^"+j+".*").matcher(""+(k));
-				if(m.find()){
-					System.out.println("match above");
-					sb=new StringBuilder();
-					sb.append(k);
-					sb.append(ext);
-					results[i++]=sb.toString();
-				}
-				k++;
-			}
-			else{
-				k=1;
-				j++;
-			}
+		int k=1;
+		StringBuilder sb ;
+		String ext=".mp3";
+		for(int i=0;i<nums.length;i++){
+			sb=new StringBuilder();
+			nums[i]=sb.append((k++)+ext).toString();
+		}
+		Arrays.sort(nums);
+		if(n<=50)return nums;
+		String [] results = new String[50];
+		for(int i=0;i<50;i++){
+			results[i]=nums[i];
 		}
 		return results;
 	}
